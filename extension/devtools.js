fd984f0ee77c0ba150d290db93ba3f21b243cb6b
Harry Allen <66224939+MajesticString@users.noreply.github.com>
2021-06-11 11:38:53 -0700

    Added opened folder icon and css preprocessers and ts icons

--- extension/devtools.js
+++ extension/devtools.js
@@ -3,3 +3,16 @@ fetch('Custom.min.css')
   .then((styles) => {
     chrome.devtools.panels.applyStyleSheet(styles);
   });
+
+// adds class to codemirror javasrcipt functions
+setInterval(() => {
+  document
+    .querySelectorAll(
+      '#console-prompt > div > div.console-prompt-editor-container div.CodeMirror-scroll > div.CodeMirror-sizer div.CodeMirror-code > div > pre span.cm-js-property'
+    )
+    .forEach((el) => {
+      if (el.nextSibling.data.trim().charAt(0) === '(') {
+        el.classList.add('cm-js-function');
+      }
+    });
+}, 1000);
